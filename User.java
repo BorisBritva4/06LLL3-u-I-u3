e82efd6a3c3c2c5fc46a2f6c3f75687e4983c021
BorisBritva4 <parmo1997@yandex.ru>
2017-03-01 09:08:04 +0300

    Update User.java

--- User.java
+++ User.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pkg06lll3.u.i.u3;
 
-/**
- *
- * @author 1
- */
+
 public class User {
  private long userid;
  private Student LinkedProfile;
