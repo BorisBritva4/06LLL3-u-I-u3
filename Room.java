4cd880f714a55a5024745f8743cb408b4293902a
BorisBritva4 <parmo1997@yandex.ru>
2017-03-01 09:06:10 +0300

    Update Room.java

--- Room.java
+++ Room.java
@@ -1,17 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pkg06lll3.u.i.u3;
 
-/**
- *
- * @author 1
- */
+
+import java.util.List;
+
 public class Room {
     private float LivingSpace;
     private long RoomNumber;
     private byte AvailiblePositions;
     private byte TotalPositions;
+    private List<Student> LinkedStudents;
 }
