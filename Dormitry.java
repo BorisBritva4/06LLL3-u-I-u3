a56084927aeee8a66c1d698b88717e73485003d5
BorisBritva4 <parmo1997@yandex.ru>
2017-03-01 09:04:03 +0300

    Update Dormitry.java

--- Dormitry.java
+++ Dormitry.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pkg06lll3.u.i.u3;
 
 import java.util.List;
 
-/**
- *
- * @author 1
- */
+
 public class Dormitry {
     private List<Student> ListOfStudents;
     private List<Room> ListOfRooms;
