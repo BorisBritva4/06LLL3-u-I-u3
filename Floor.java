a5e8041e51ce3829a1defa75863362e9d5cb38ea
BorisBritva4 <parmo1997@yandex.ru>
2017-03-01 09:04:39 +0300

    Update Floor.java

--- Floor.java
+++ Floor.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pkg06lll3.u.i.u3;
 
 import java.util.List;
 
-/**
- *
- * @author 1
- */
 public class Floor {
     public byte FloorNumber;
     public List<Room> LinkedRooms;
