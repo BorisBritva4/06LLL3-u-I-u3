58eae4855d2ee2a034fee454fe24b347c84ba92c
BorisBritva4 <parmo1997@yandex.ru>
2017-03-01 09:07:19 +0300

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pkg06lll3.u.i.u3;
 
-/**
- *
- * @author 1
- */
 public class Student {
  private Room LinkedRoom;
  private long id;
